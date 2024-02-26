0a658408ee32f961c1d83b3a8dbd36e68b41b813
Borna <bpinteric@pmfst.hr>
2024-02-26 13:08:36 +0100

    Final version

---
+++ Vjezbe/Vjezbe_1/zad3.py
@@ -0,0 +1,33 @@
+check=False
+pokusaj=False
+while check==False:
+    if pokusaj==True:
+        print("Pokusaj ponovno")
+    c=input("Upisi u formatu x,y koordinate prve tocke")
+    if len(c)!=2:
+        check=False
+    elif isinstance(c[0],int)==False or isinstance(c[1],int)==False:
+        check=False
+    else:
+        check=True
+    pokusaj=True
+check=False
+pokusaj=False
+while check==False:
+    if pokusaj==True:
+        print("Pokusaj ponovno")
+    d=input("Upisi u fomatu x,y koordinate druge tocke")
+    if len(d)!=2:
+        check=False
+    elif isinstance(d[0],int)==False or isinstance(d[1],int)==False:
+        check=False
+    else:
+        check=True
+    pokusaj=True
+
+
+a=tuple(int(item) for item in c)
+b=tuple(int(item) for item in d)
+k=(float(b[1])-float(a[1]))/(float(b[0])-float(a[0]))
+l=float(a[1])-k*float(a[0])
+print ("Pravac je y={}x+{}".format(k,l))
