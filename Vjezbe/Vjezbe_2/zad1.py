3298157bf7dc0a2be6175d493a3ca45b27a6d539
JoskoPuljak <jpuljak@pmfst.hr>
2024-03-01 18:15:21 +0100

    Prva verzija koja radi

--- Vjezbe/Vjezbe_2/zad1.py
+++ Vjezbe/Vjezbe_2/zad1.py
@@ -1,23 +1,30 @@
-import matplotlib as plt
+#moduli
+import matplotlib.pyplot as plt
 import numpy as np
+#pocetni uvijeti
 v0=0
 s0=0
 F=10
 m=2
+#a/t graf
 t=np.linspace(0,10,100)
-a=F/m
-
-deltav=a*0.1
-v=np.array([i*deltav for i in range (0,101)])
-deltas=v*0.1
-slista=[]
-svrijednost=s0
-for i in deltas:
-    svrijednost+=i
-    slista.append(svrijednost)
-s=np.array(slista)
-
-plt.plot(a,t)
-plt.plot(v,t)
-plt.plot(s,t)
+a_val=F/m
+a=np.array([a_val for i in t])
+plt.subplot(3,1,1)
+plt.ylabel("a/m/s^2")
+plt.title("a/t graf")
+plt.plot(t,a,color="purple")
+#v/t graf
+v=a_val*t
+plt.subplot(3,1,2)
+plt.ylabel("v/m/s")
+plt.title("v/t graf")
+plt.plot(t,v,color="pink")
+#x/t graf
+x=np.array([(i*(j**2))/2 for i,j in zip(a,t)])
+plt.subplot(3,1,3)
+plt.plot(t,x,color="blue")
+plt.ylabel("x/m")
+plt.xlabel("t/s")
+plt.title("x/t graf")
 plt.show()
