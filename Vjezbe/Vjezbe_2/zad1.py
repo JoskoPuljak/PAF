253afdba7eed733861da5ce7f66930170d12203a
JoskoPuljak <jpuljak@pmfst.hr>
2024-03-01 18:20:37 +0100

    Prva verzija koja radi

--- Vjezbe/Vjezbe_2/zad1.py
+++ Vjezbe/Vjezbe_2/zad1.py
@@ -2,10 +2,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 #pocetni uvijeti
-v0=0
-s0=0
-F=10
-m=2
+F=int(input("Upiši silu u Njutnima"))
+m=int(input("Upiši masu u kilogramima"))
 #a/t graf
 t=np.linspace(0,10,100)
 a_val=F/m
