88f7b1b90fe0d924d86290e3a0392672378233ad
JoskoPuljak <jpuljak@pmfst.hr>
2024-03-04 15:33:40 +0100

    Radna verzija

--- Vjezbe/Vjezbe_2/zad1_numericki.py
+++ Vjezbe/Vjezbe_2/zad1_numericki.py
@@ -2,31 +2,35 @@
 import matplotlib.pyplot as plt
 import numpy as np
 #pocetni uvijeti
-F=int(input("Upisi silu u Njutnima"))
-m=int(input("Upisi masu u kilogramima"))
-#a/t graf
-t=np.linspace(0,10,100)
-a_val=F/m
-a=np.array([a_val for i in t])
-plt.subplot(3,1,1)
-plt.ylabel("a/m/s^2")
-plt.title("a/t graf")
-plt.plot(t,a,color="purple")
-#v/t graf
-dt=0.1
-dv=a*dt
-v=[dv*i for i in range(0,100)]
+def zad1(F,m):
+    #a/t graf
+    a_val=F/m
+    dt=0.1
+    t=[0.1*i for i in range (0,101)]
+    a=[a_val for i in range(0,101)]
+    plt.subplot(3,1,1)
+    plt.ylabel("a/m/s^2")
+    plt.title("a/t graf")
+    plt.plot(t,a,color="purple")
+    #v/t graf
+    dv=a_val*dt
+    v=[dv*i for i in range (0,101)]
+    plt.subplot(3,1,2)
+    plt.ylabel("v/m/s")
+    plt.title("v/t graf")
+    plt.plot(t,v,color="pink")
+    #x/t graf
+    dx=[i*dt for i in v]
+    x=[]
+    sum=0
+    for i in dx:
+        sum+=i
+        x.append(sum)
+    plt.subplot(3,1,3)
+    plt.plot(t,x,color="blue")
+    plt.ylabel("x/m")
+    plt.xlabel("t/s")
+    plt.title("x/t graf")
+    plt.show()
 
-print(v)
-plt.subplot(3,1,2)
-plt.ylabel("v/m/s")
-plt.title("v/t graf")
-plt.plot(t,v,color="pink")
-#x/t graf
-
-plt.subplot(3,1,3)
-plt.plot(t,x,color="blue")
-plt.ylabel("x/m")
-plt.xlabel("t/s")
-plt.title("x/t graf")
-plt.show()
+zad1(20,5)
