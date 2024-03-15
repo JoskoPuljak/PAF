28800d3b049ce01cc1030072ace8653d03a1072d
JoskoPuljak <jpuljak@pmfst.hr>
2024-03-15 12:27:05 +0100

    update

--- Vjezbe/Vjezbe_3/linregress.py
+++ Vjezbe/Vjezbe_3/linregress.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-def lin_regress(phi,M):
+def lin_regress(phi,M,colour="red",xlabel="x os", ylabel="y os", title="x/y graf"):
     prod_list=[i*j for i,j in zip(phi,M)]
     product_mean=np.mean(prod_list)
     x_square_list=[i**2 for i in phi]
@@ -9,7 +9,10 @@ def lin_regress(phi,M):
     kx=[slope*i for i in phi]
     
     
-    plt.plot(phi,kx)
+    plt.plot(phi,kx,color=colour)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
     plt.show()
 
 
