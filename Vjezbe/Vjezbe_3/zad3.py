d0ebb01ff31d6e5f42c4535c1aab87d3a3d4ead8
JoskoPuljak <jpuljak@pmfst.hr>
2024-03-15 13:21:03 +0100

    final version

--- Vjezbe/Vjezbe_3/zad3.py
+++ Vjezbe/Vjezbe_3/zad3.py
@@ -9,7 +9,7 @@ import numpy as np
 print(np.mean([1,2,2,2,3,4,5,6,7,8]))
 print(np.std([1,2,2,2,3,4,5,6,7,8]))
 
-#standardna devijacija u numpyu je razlicita od standardne devijacije u našoj funkciji jer je ona napravljena za velik broj podatak, stoga je možemo podijeliti s n-1
+#standardna devijacija u numpyu je razlicita od standardne devijacije u našoj funkciji jer je ona napravljena za velik broj podataka, stoga je možemo podijeliti s n-1
 print(np.std([1,2,2,2,3,4,5,6,7,8])/mt.sqrt(len([1,2,2,2,3,4,5,6,7,8])-1))
 #dobijemo isti kao i u našem modulu
 
