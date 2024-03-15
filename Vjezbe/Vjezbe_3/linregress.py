b51a73ecf63bb6cea7b8806eac81189e9d1c9b3d
JoskoPuljak <jpuljak@pmfst.hr>
2024-03-15 12:37:59 +0100

    update

--- Vjezbe/Vjezbe_3/linregress.py
+++ Vjezbe/Vjezbe_3/linregress.py
@@ -1,18 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
-def lin_regress(phi,M,colour="red",xlabel="x os", ylabel="y os", title="x/y graf"):
-    prod_list=[i*j for i,j in zip(phi,M)]
+def lin_regress(x,y,colour="red",xlabel="x os", ylabel="y os", title="x/y graf",linelegend="graf linearne regresije",scatterlegend="pravi podaci"):
+    prod_list=[i*j for i,j in zip(x,y)]
     product_mean=np.mean(prod_list)
-    x_square_list=[i**2 for i in phi]
+    x_square_list=[i**2 for i in x]
     x_square_mean=np.mean(x_square_list)
     slope=product_mean/x_square_mean
-    kx=[slope*i for i in phi]
+    kx=[slope*i for i in x]
     
     
-    plt.plot(phi,kx,color=colour)
+    plt.plot(x,kx,color=colour)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.title(title)
+    plt.scatter(x,y)
+    plt.legend([linelegend,scatterlegend])
     plt.show()
 
 
--- Vjezbe/Vjezbe_3/zad4.py
+++ Vjezbe/Vjezbe_3/zad4.py
@@ -1,4 +1,4 @@
 import linregress as lin
 M = [0.052, 0.124, 0.168, 0.236, 0.284, 0.336]
 phi = [0.1745, 0.3491, 0.5236, 0.6981, 0.8727, 1.0472]
-lin.lin_regress(M,phi,"purple", "phi/rad", "M/Nm","M/phi graf")
+lin.lin_regress(M,phi,"purple", "phi/rad", "M/Nm","M/phi graf","pravac linearne regresije M/phi", "prave vrijednosti M")
