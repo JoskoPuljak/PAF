8e6c1b088e3915e9216d6e31648617af5edb4403
JoskoPuljak <jpuljak@pmfst.hr>
2024-04-05 13:27:27 +0200

    Final version

--- Vjezbe/Vjezbe_4/particle.py
+++ Vjezbe/Vjezbe_4/particle.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 class Particle:
     def __init__(self,v_0,kut,x_0,y_0):
+        self.x=[x_0]
+        self.y=[y_0]
         self.vx=v_0*np.cos(np.radians(kut))
         self.vy=[v_0*np.sin(np.radians(kut))]
         self.kut=kut
-        self.x=[x_0]
-        self.y=[y_0]
         self.t=[0]
     def reset(self):
         self.vy=[self.vy[0]]
@@ -17,8 +17,8 @@ class Particle:
         g=9.81
         self.t.append(self.t[-1] +dt)
         self.x.append(self.x[-1]+self.vx*dt)
-        self.vy.append(self.vy[-1]-g*dt)
         self.y.append(self.y[-1]+self.vy[-1]*dt)
+        self.vy.append(self.vy[-1]-g*dt)
     def range(self,dt):
         while self.y[-1]>=0:
             self.__move(dt)
