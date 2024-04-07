c62e41a2425ad3f37aa389df3cfd63ccd47196ce
JoškoPuljak <jpuljak@pmfst.hr>
2024-04-07 19:51:39 +0200

    Final version

--- Vjezbe/Vjezbe_5/zad2.py
+++ Vjezbe/Vjezbe_5/zad2.py
@@ -5,10 +5,18 @@ import matplotlib.pyplot as plt
 raspon=range(10,50)
 Prav_donja=[clc.pravokutna_integracija(lambda a:2*a**2+3,0,1,i)[0] for i in raspon] 
 Prav_gornja=[clc.pravokutna_integracija(lambda a:2*a**2+3,0,1,i)[1] for i in raspon]
-Prav_trap=[clc.trapezna_integracija(lambda a:2*a**2+3,0,1,i) for i in raspon]
+Trap=[clc.trapezna_integracija(lambda a:2*a**2+3,0,1,i) for i in raspon]
+#analiticki
+Analiticko=(2/3)*1**3+3*1
+Analitic_list=[Analiticko for i in raspon] 
 
+#grafovi
 plt.scatter(list(raspon),Prav_donja,color="blue")
 plt.scatter(list(raspon),Prav_gornja,color="red")
-plt.scatter(list(raspon),Prav_trap,color="purple")
-#plt.plot(list(raspon),[(2*a**3)/3+ 3*a for a in ])
+plt.scatter(list(raspon),Trap,color="purple")
+plt.scatter(list(raspon),Analitic_list,color="orange",s=5)
+plt.xlabel("broj koraka")
+plt.ylabel("vrijednost integrala")
+plt.title("graf vrijednosti integrala u ovisnosti o broju koraka")
+plt.legend(["Donja meda pravokutne integracije","Gornja međa pravokutne integracije","Trapezna interacija","Analiticko izračunata vrijednost"])
 plt.show()
