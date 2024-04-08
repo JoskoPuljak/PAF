e233790bacbe8ec4037cca63bb532b53141852f9
JoskoPuljak <jpuljak@pmfst.hr>
2024-04-08 16:56:31 +0200

    prva verzija

---
+++ Vjezbe/Vjezbe_6/harmonic_oscillator.py
@@ -0,0 +1,42 @@
+#moduli
+import numpy as np
+import matplotlib.pyplot as plt
+#klasa
+class HarmonicOscillator:
+    #početni uvjeti
+    def __init__(self,k,m,v0,x0):
+        self.k=k
+        self.m=m
+        self.t=[0]
+        self.x=[x0]
+        self.v=[v0]
+        self.a=[-self.k/self.m*x0]
+    #resetiranje lista
+    def reset(self):
+        self.t=self.t[0]
+        self.y=self.x[0]
+        self.v=self.v[0]
+        self.a=self.a[0]
+    #punjenje lista
+    def oscillate(self,t,dt):
+        for i in np.arange(0,t,dt):
+            self.t.append(self.t[-1]+dt)
+            self.a.append(-self.k/self.m*self.x[-1])
+            self.v.append(self.v[-1]+(self.a[-1]*dt))
+            self.x.append(self.x[-1]+(self.v[-1]*dt))
+            
+    #crtanje        
+    def plot_trajectory(self):
+        plt.subplot(1,3,1)
+        plt.plot(self.t,self.x,color="blue")
+        plt.subplot(1,3,2)
+        plt.plot(self.t,self.v,color="green")
+        plt.subplot(1,3,3)
+        plt.plot(self.t,self.a,color="purple")
+        plt.show()
+
+
+
+
+
+
