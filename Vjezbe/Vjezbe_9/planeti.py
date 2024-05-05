36c3af8db7751548996cc1e38046a67f1f4ace38
JoškoPuljak <jpuljak@pmfst.hr>
2024-05-06 00:47:12 +0200

    skoro

--- Vjezbe/Vjezbe_9/planeti.py
+++ Vjezbe/Vjezbe_9/planeti.py
@@ -10,7 +10,7 @@ class dvaplaneta:
         self.v_Zemlja=[np.array((0,v0_Zemlja))]
         self.v_Sunce=[np.array((0,0))]
         #self.a_Sunce=[(-self.G*(self.Mz)/abs(np.linalg.norm(self.x_Sunce)-np.linalg.norm(self.x_Zemlja))**3)*(np.linalg.norm(self.x_Sunce)-np.linalg.norm(self.x_Zemlja[0][0]))]
-        self.a_Zemlja=[(-self.G*(self.Ms)/(np.linalg.norm(self.x_Zemlja[0]-self.x_Sunce[0]))**3)*(self.x_Sunce[0]-self.x_Zemlja[0])]
+        self.a_Zemlja=[(-self.G*(self.Ms)/(np.linalg.norm(self.x_Zemlja[0]-self.x_Sunce[0]))**3)*(self.x_Zemlja[0]-self.x_Sunce[0])]
     def reset(self):
         self.x_Sunce=[self.x_Sunce[0]]
         self.x_Zemlja=[self.x_Zemlja[0]]
@@ -24,20 +24,22 @@ class dvaplaneta:
         #self.x_Sunce.append(np.array((self.x_Sunce[-1][0]+self.v_Sunce[-1][0]*dt,self.x_Sunce[-1][1]+self.v_Sunce[-1][1]*dt)))
         self.x_Zemlja.append(self.x_Zemlja[-1]+self.v_Zemlja[-1]*dt)
         #self.a_Sunce.append(np.array(((-self.G*(self.Mz)/abs(self.x_Sunce[-1][0]-self.x_Zemlja[-1][0])**3)*(self.x_Sunce[-1][0]-self.x_Zemlja[-1][0]),(-self.G*(self.Mz)/abs(self.x_Sunce[-1][1]-self.x_Zemlja[-1][1])**3)*(self.x_Sunce[-1][1]-self.x_Zemlja[-1][1]))))
-        self.a_Zemlja.append((-self.G*(self.Ms)/(np.linalg.norm(self.x_Zemlja[-1]-self.x_Sunce[-1]))**3)*(self.x_Sunce[-1]-self.x_Zemlja[-1]))
+        self.a_Zemlja.append((-self.G*(self.Ms)/(np.linalg.norm(self.x_Zemlja[-1]-self.x_Sunce[-1]))**3)*(self.x_Zemlja[-1]-self.x_Sunce[-1]))
+        
     def plot_trajectory(self,dt,t):
-        for i in np.arange(dt,t,dt):
+        for i in np.arange(0,t,dt):
             self.__move(dt)
         xs=self.x_Sunce[0][0]
         ys=self.x_Sunce[0][1]
-        xz=self.x_Zemlja[:][0]
-        yz=self.x_Zemlja[:][1]
+        xz=np.array(self.x_Zemlja)[:,0]
+        yz=np.array(self.x_Zemlja)[:,1]
         plt.plot(xs,ys,color="yellow")
         plt.plot(xz,yz,color="blue")
         plt.show()
+        print(xz)
 Sunceizemlja=dvaplaneta((0,0),(1.486*(10**11),0),29783)
 #Sunceizemlja.move(10)
 #print(Sunceizemlja.v_Sunce)
 #print(Sunceizemlja.v_Zemlja)
-Sunceizemlja.plot_trajectory(1000,31556909)
-print(Sunceizemlja.v_Zemlja)
+Sunceizemlja.plot_trajectory(100000,31556909)
+
