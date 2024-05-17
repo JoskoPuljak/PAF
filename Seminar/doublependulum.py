c0e1c05a786ed2324c8ff6fe7c5653074dfaa825
JoskoPuljak <jpuljak@pmfst.hr>
2024-05-17 13:34:33 +0200

    working version

--- Seminar/doublependulum.py
+++ Seminar/doublependulum.py
@@ -38,7 +38,6 @@ class duplo_njihalo:
     def plot_trajectory(self,dt,t):
         for i in np.arange(0,t,dt):
             self.move(dt)
-        #print(self.x1)
         x1=np.array(self.x1)[:,0]
         y1=np.array(self.x1)[:,1]
         x2=np.array(self.x2)[:,0]
@@ -47,15 +46,20 @@ class duplo_njihalo:
         plt.rcParams["figure.figsize"] = [5, 5]
         plt.rcParams["figure.autolayout"] = True
         fig=plt.figure()
-        ax=plt.axes(xlim=(-0.1,0.1),ylim=(-0.1,0.1))
-
+        ax=plt.axes(xlim=(-0.1,0.1),ylim=(-0.12,0.1))
+        fulcrum=ax.scatter([0],[0],color="black")
+        
         line,=ax.plot([],[],"o",color="blue")
         line2,=ax.plot([],[],"o",color="blue")
+        line3,=ax.plot([],[],color="black")
+        line4,=ax.plot([],[],color="black")
         
         def init():
             line.set_data([],[])
             line2.set_data([],[])
-            return line,line2
+            line3.set_data([],[])
+            line4.set_data([],[])
+            return line,line2,line3,line4
 
         def animate(i):
             x_1=x1[i]
@@ -64,12 +68,75 @@ class duplo_njihalo:
             x_2=x2[i]
             y_2=y2[i]
             line2.set_data(x_2,y_2)
-            return line,line2
+            x_3=np.array([0,x1[i]])
+            y_3=np.array([0,y1[i]])
+            line3.set_data(x_3,y_3)
+            x_4=np.array([x1[i],x2[i]])
+            y_4=np.array([y1[i],y2[i]])
+            line4.set_data(x_4,y_4)
+            return line3,line4,line,line2
         anim = ani.FuncAnimation(fig, animate, init_func=init, frames=100000, interval=50, blit=True)
         
         plt.show()
+    def plot_trace(self,dt,t):
+        for i in np.arange(0,t,dt):
+            self.move(dt)
+        x1=np.array(self.x1)[:,0]
+        y1=np.array(self.x1)[:,1]
+        x2=np.array(self.x2)[:,0]
+        y2=np.array(self.x2)[:,1]
+        
+        plt.rcParams["figure.figsize"] = [5, 5]
+        plt.rcParams["figure.autolayout"] = True
+        fig=plt.figure()
+        ax=plt.axes(xlim=(-0.1,0.1),ylim=(-0.12,0.1))
+        fulcrum=ax.scatter([0],[0],color="black")
+        
+        line,=ax.plot([],[],"o",color="blue")
+        line2,=ax.plot([],[],"o",color="blue")
+        line3,=ax.plot([],[],color="black")
+        line4,=ax.plot([],[],color="black")
+        line5,=ax.plot([],[],color="green")
+        
+        def init():
+            line.set_data([],[])
+            line2.set_data([],[])
+            line3.set_data([],[])
+            line4.set_data([],[])
+            line5.set_data([],[])
+            return line,line2,line3,line4,line5
+
+        def animate(i):
+            x_1=x1[i]
+            y_1=y1[i]
+            line.set_data(x_1,y_1)
+            x_2=x2[i]
+            y_2=y2[i]
+            line2.set_data(x_2,y_2)
+            x_3=np.array([0,x1[i]])
+            y_3=np.array([0,y1[i]])
+            line3.set_data(x_3,y_3)
+            x_4=np.array([x1[i],x2[i]])
+            y_4=np.array([y1[i],y2[i]])
+            line4.set_data(x_4,y_4)
+            x_5=x2[:i]
+            y_5=y2[:i]
+            line5.set_data(x_5,y_5)
+            return line5,line3,line4,line,line2
+        anim = ani.FuncAnimation(fig, animate, init_func=init, frames=100000, interval=5, blit=True)
+        
+        plt.show()
+    def plot_picture(self,dt,t):
+        for i in np.arange(0,t,dt):
+            self.move(dt)
+        x1=np.array(self.x1)[:,0]
+        y1=np.array(self.x1)[:,1]
+        x2=np.array(self.x2)[:,0]
+        y2=np.array(self.x2)[:,1]
+        plt.plot(x2,y2)
+        plt.show()
 
 
-Njihy=duplo_njihalo(0.1,0.1,0.05,0.05,40,20,0.5,0.1)
-Njihy.plot_trajectory(0.01,15)
+
+
 
